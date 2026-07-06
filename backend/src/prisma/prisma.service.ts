3cc6eda2f4368a2ddaadb0e7c0c9462f76b9d712
unknown <hafizhafrienda2005@gmail.com>
2026-07-06 10:51:29 +0700

    Generate Prisma Accel

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,13 +1,6 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient {}
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-}
